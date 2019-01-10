f95fc3745fd46cf8d78cce1d720ecdc40ce04943
eselbeus <eselbeus@gmail.com>
2019-01-10 03:32:47 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -2,8 +2,8 @@ def my_collect(arr)
   return_arr = []
   i = 0 
   while i < arr.length 
-    yield arr[i]
-    return_arr << arr[i]
+    current = yield arr[i]
+    return_arr << current
     i += 1
   end
   return_arr
